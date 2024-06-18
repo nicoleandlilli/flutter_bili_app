a7945f8ae8cab3bc0f5da032f7975a02bc628a14
wangguiye <wangguiye@lenovo.com>
2024-06-18 15:38:44 +0800

    添加统一网络层返回格式

---
+++ lib/http/core/hi_net_adapter.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import '../request/base_request.dart';
+
+
+abstract class HiNetAdapter {
+  Future<HiNetResponse<T>> send<T>(BaseRequest request);
+}
+
+///统一网络层返回格式
+class HiNetResponse<T>{
+  HiNetResponse(
+    {this.data,
+    this.request,
+      this.statusCode,
+      this.statusMessage,
+      this.extra});
+
+  T? data;
+  BaseRequest? request;
+  int? statusCode;
+  String? statusMessage;
+  dynamic extra;
+
+  @override
+  String toString() {
+    if(data is Map){
+      return json.encode(data);
+    }
+    return super.toString();
+  }
+}
