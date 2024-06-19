dd68b41af1d4546f6aaed82929d657cf07806059
wangguiye <wangguiye@lenovo.com>
2024-06-20 03:16:02 +0800

    添加注册界面

--- lib/page/registration_page.dart
+++ lib/page/registration_page.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bili_app/widget/appbar.dart';
 import 'package:flutter_bili_app/widget/login_effect.dart';
 
+import '../http/core/hi_error.dart';
+import '../http/dao/login_dao.dart';
+import '../util/string_util.dart';
 import '../widget/login_input.dart';
 
 class RegistrationPage extends StatefulWidget{
@@ -90,7 +94,7 @@ class _RegistrationPageState extends State<RegistrationPage>{
               true,
               keyboardType: TextInputType.number,
               onChanged: (text) {
-                imoocId=text;
+                orderId=text;
                 checkInput();
               },
             ),
@@ -104,15 +108,86 @@ class _RegistrationPageState extends State<RegistrationPage>{
   }
 
   void checkInput() {
+    bool enable;
+    if(isNotEmpty(userName!) &&
+        isNotEmpty(password) &&
+        isNotEmpty(rePassword) &&
+        isNotEmpty(imoocId) &&
+        isNotEmpty(orderId)){
+      enable = true;
+    }else{
+      enable = false;
+    }
+    setState(() {
+      loginEnable = enable;
+    });
   }
 
   _loginButton() {
     return InkWell(
       onTap: (){
-
+        if(loginEnable){
+          // send();
+          checkParams();
+        }else{
+          if (kDebugMode) {
+            print('loginEnable is false');
+          }
+        }
       },
-      child: Text('注册'),
+      child: const Text('注册'),
     );
+  }
+
+  void send() async{
+    try {
+      var result = await LoginDao.registration(
+          userName!, password!, imoocId!, orderId!);
+
+      // if(result['0'] == 0){
+      //   print('注册成功');
+      //   if(widget.onJumpToLogin != null){
+      //     widget.onJumpToLogin();
+      //   }
+      // }else{
+      // print(result['msg']);
+      // }
+
+      if(result != null){
+        if (kDebugMode) {
+          print('注册成功');
+        }
+        if(widget.onJumpToLogin != null){
+          widget.onJumpToLogin();
+        }
+      }
+
+      if (kDebugMode) {
+        print(result);
+      }
+    }on NeedAuth catch(e){
+      if (kDebugMode) {
+        print(e);
+      }
+    }on HiNetError catch(e){
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  void checkParams() {
+    String? tips;
+    if(password != rePassword){
+      tips = '两次密码不一致';
+    }else if(orderId?.length != 4){
+      tips = '请输入订单号的后四位';
+    }
+    if(tips != null){
+      print(tips);
+      return;
+    }
+    send();
   }
 
 }
