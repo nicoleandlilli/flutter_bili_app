bf76fad33f10af251ababc32e88a2b9da5e4b25e
wangguiye <wangguiye@lenovo.com>
2024-06-20 05:18:10 +0800

    创建路由封装类

---
+++ lib/navigator/hi_navigator.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bili_app/page/login_page.dart';
+import 'package:flutter_bili_app/page/registration_page.dart';
+///创建界面
+pageWrap(Widget child){
+  return MaterialPage(key: ValueKey(child.hashCode),child: child);
+}
+
+///自定义路由封装，路由状态
+enum RouteStatus { login, registration, home, detail, unknown}
+
+///获取page对应的RouteStatus
+RouteStatus getStatus(MaterialPage page){
+  if(page.child is LoginPage){
+    return RouteStatus.login;
+  }else if(page.child is RegistrationPage){
+    return RouteStatus.registration;
+  }
+  // else if(page.child is HomePage){
+  //   return RouteStatus.home;
+  // }else if(page.child is VideoDetailPage){
+  //   return RouteStatus.detail;
+  // }
+  else{
+    return RouteStatus.unknown;
+  }
+}
+
+///路由信息
+class RouteStatusInfo{
+  final RouteStatus routeStatus;
+  final Widget page;
+
+  RouteStatusInfo(this.routeStatus, this.page);
+}
