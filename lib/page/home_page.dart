e21d37be01026cd4b4df4dc3b05d206bbd77aa58
wangguiye <wangguiye@lenovo.com>
2024-06-21 08:30:41 +0800

    首页添加顶部的tabbar

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -2,7 +2,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bili_app/main.dart';
 import 'package:flutter_bili_app/navigator/hi_navigator.dart';
-
+import 'package:flutter_bili_app/util/color.dart';
+import 'package:underline_indicator/underline_indicator.dart';
 import '../model/video_model.dart';
 
 
@@ -14,11 +15,15 @@ class HomePage extends StatefulWidget {
   
 }
 
-class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin{
+class _HomePageState extends State<HomePage>
+    with AutomaticKeepAliveClientMixin,TickerProviderStateMixin{
   var listener;
+  late TabController _controller;
+  var tabs = ["推荐","热门", "追播","影视","搞笑","日常","综合","手机游戏","短片-手书-配音"];
   @override
   void initState() {
     super.initState();
+    _controller=TabController(length: tabs.length, vsync: this);
     HiNavigator.getInstance().addListener(listener = (current, pre) {
       if (kDebugMode) {
         print('home:current:${current.page}');
@@ -50,6 +55,11 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin{
      body: Container(
        child: Column(
          children: [
+           Container(
+             color: Colors.white,
+             padding: EdgeInsets.only(top: 30),
+             child: _tabBar(),
+           ),
            const Text('首页'),
            MaterialButton(
                onPressed: () {
@@ -69,5 +79,24 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin{
 
   @override
   bool get wantKeepAlive => true;
+
+  _tabBar() {
+    return TabBar(
+      controller: _controller,
+        isScrollable: true,
+        labelColor: Colors.black,
+        indicator: const UnderlineIndicator(
+            strokeCap: StrokeCap.round,
+            borderSide: BorderSide(color: primary,width: 3),
+            insets: EdgeInsets.only(left: 15,right: 15),
+        ),
+    tabs: tabs.map<Tab>((tab) {
+      return Tab(child: Padding(
+        padding: const EdgeInsets.only(left: 5,right: 5),
+        child: Text(tab,style: TextStyle(fontSize: 16),),
+      ),);
+    }).toList(),
+    );// tabs: (tabs));
+  }
   
 }
---
+++ lib/page/home_tab_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeTabPage extends StatefulWidget{
+  @override
+  _HomeTabPageState createState() => _HomeTabPageState();
+
+}
+
+class _HomeTabPageState extends State<HomeTabPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("widget"),
+    );
+  }
+
+}
