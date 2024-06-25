97d7651c287f2863e3d1edb0089cb2713d256d57
wangguiye <wangguiye@lenovo.com>
2024-06-26 05:48:38 +0800

    VideoDetailPage类中方法名更改

--- lib/page/video_detail_page.dart
+++ lib/page/video_detail_page.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bili_app/model/home_mo.dart';
 import 'package:flutter_bili_app/widget/video_view.dart';
@@ -13,11 +11,11 @@ class VideoDetailPage extends StatefulWidget{
   const VideoDetailPage({super.key, required this.videoMo});
   // args: {'videoMo':VideoMo(tid:bannerMo.tid)}
   @override
-  _VideoDetailPageState createState() => _VideoDetailPageState();
+  VideoDetailPageState createState() => VideoDetailPageState();
 
 }
 
-class _VideoDetailPageState extends State<VideoDetailPage> {
+class VideoDetailPageState extends State<VideoDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
