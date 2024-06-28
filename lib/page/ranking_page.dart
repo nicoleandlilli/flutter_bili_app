58a2c8d604918c27069d0d6dd2ac5ce38fe40053
wangguiye <wangguiye@lenovo.com>
2024-06-28 08:03:31 +0800

    排行榜界面框架搭建

--- lib/page/ranking_page.dart
+++ lib/page/ranking_page.dart
@@ -1,23 +1,86 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bili_app/util/view_util.dart';
+import 'package:flutter_bili_app/widget/hi_tab.dart';
+
+import '../widget/navigation_bar.dart';
 
 class RankingPage extends StatefulWidget{
   const RankingPage({super.key});
 
   @override
-  _RankingPageState createState() => _RankingPageState();
+  RankingPageState createState() => RankingPageState();
 
 }
 
 
-class _RankingPageState extends State<RankingPage> {
+class RankingPageState extends State<RankingPage> with TickerProviderStateMixin{
+  static const tabs = [
+    {"key": "like","name": "最热"},
+    {"key": "pub date","name": "最新"},
+    {"key": "favorite","name": "收藏"},
+  ];
+
+  late TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller=TabController(length: tabs.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        child: const Text('排行'),
+      body: Column(
+        children: [
+          _buildNavigationBar(),
+          _buildTabView(),
+        ],
       ),
+    );
+  }
+
+  _buildNavigationBar() {
+    return CNavigationBar(
+      child: Container(
+        alignment: Alignment.center,
+        child: _tabBar(),
+        decoration: bottomBoxShadow(),
+      ),
+
+    );
+  }
+
+  _tabBar() {
+    return HiTab(
+      tabs.map<Tab>((tab) {
+        return Tab(
+          text: tab['name'],
+        );
+      }).toList(),
+      fontSize: 16,
+      borderWidth: 3,
+      unselectedLabelColor: Colors.black54,
+      controller: _controller,
+    );
+  }
+
+  _buildTabView() {
+    return Flexible(
+        child: TabBarView(
+          controller: _controller,
+          children: tabs.map((tab) {
+            return Text(tab['name']!);
+          }).toList(),
+
+        ),
     );
   }
 
